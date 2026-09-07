e1879e9fcf200463bf04f65c059bc36e83d8e330
brayanjavierrodriguez2007FEI <brayanjavierrodriguez2007@gmail.com>
2026-09-06 22:33:39 -0300

    Sim

--- Treinar/condicionais_relacionais/Calcular_idade.py
+++ Treinar/condicionais_relacionais/Calcular_idade.py
@@ -2,6 +2,8 @@ ano_atual = int(input('Qual é o ano atual?: '))
 ano_nascimento = int(input('Qual é seu ano de nascimento?: '))
 idade = ano_atual - ano_nascimento
 if idade >=18:
-    print("Você pode tirar a sua CNH!")
+    print ('A sua idade é: ', idade)
+    print ('Você pode tirar a sua CNH!')
 else:
+    print ('A sua idade é: ', idade)
     print ("Você não pode tirar a sua CNH.")
